5b9b4b8338f1829eac719c3793d8409542340e34
GlenMcNeilley <glenmcn@gmail.com>
2011-02-23 12:10:37 -0700

    add early isCancelled/setCancelled check/set

--- src/com/LandOfGlendria/HunkleberryGeneral/HGPlayerListener.java
+++ src/com/LandOfGlendria/HunkleberryGeneral/HGPlayerListener.java
@@ -6,64 +6,60 @@ import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.plugin.Plugin;
 
-public class HGPlayerListener extends PlayerListener
-{
+public class HGPlayerListener extends PlayerListener {
 
 	private final Logger log = Logger.getLogger("Minecraft");
 	private static HGCommandHandler commandHandler;
 	private HGMessageManagement msg;
 
-	public HGPlayerListener(Plugin plugin, HGMessageManagement msg)
-	{
+	public HGPlayerListener(Plugin plugin, HGMessageManagement msg) {
 		this.msg = msg;
 		commandHandler = new HGCommandHandler(plugin, msg);
 	}
 
-	public void onPlayerCommand(PlayerChatEvent event)
-	{
+	public void onPlayerCommand(PlayerChatEvent event) {
 		String split[] = event.getMessage().split(" ");
 		Player player = event.getPlayer();
-		if(split[0].equalsIgnoreCase("/hg_test"))
-		{
-			msg.sendPositiveMessage(player, (new StringBuilder(String.valueOf(HunkleberryGeneral.pdfFile.getName()))).append(" version ").append(HunkleberryGeneral.pdfFile.getVersion()).append(" is listening.").toString());
+		if (split[0].equalsIgnoreCase("/hg_test")) {
+			msg.sendPositiveMessage(
+					player,
+					(new StringBuilder(String.valueOf(HunkleberryGeneral.pdfFile.getName()))).append(" version ")
+							.append(HunkleberryGeneral.pdfFile.getVersion()).append(" is listening.").toString());
 			event.setCancelled(true);
 			return;
 		}
 		String base = split[0].substring(1);
 		HGCommandData command = HGCommandData.getCommandDataByName(base);
-		if(command == null)
-		{
+		if (command == null) {
 			return;
 		}
-		if(!command.getServerAllowed().booleanValue())
-		{
+		if (event.isCancelled()) {
+			log.warning((new StringBuilder(String.valueOf(HunkleberryGeneral.pdfFile.getName()))).append(": /").append(base)
+					.append(" Another plugin is listening for this command, consider creating an alias.").toString());
+		} else {
+			event.setCancelled(true);
+		}
+		if (!command.getServerAllowed().booleanValue()) {
 			return;
 		}
-		if(!player.isOp() && command.getOpsOnly().booleanValue())
-		{
-			if(HGConfig.permissions == null)
-			{
+		if (!player.isOp() && command.getOpsOnly().booleanValue()) {
+			if (HGConfig.permissions == null) {
 				return;
 			}
-			if(!HGConfig.permissions.has(player, command.getPermissions()))
-			{
+			if (!HGConfig.permissions.has(player, command.getPermissions())) {
 				return;
 			}
 		}
 		String error;
-		if((error = commandHandler.handleCommand(player, command, split)) != null)
-		{
+		if ((error = commandHandler.handleCommand(player, command, split)) != null) {
 			String helpCommand = HGCommandData.HG_HELP.getCommand();
 			msg.sendColorErrorUsage(player, error, command.getCommand(), command.getCommandArgs(), helpCommand);
 		}
-		if(event.isCancelled())
-		{
-			log.warning((new StringBuilder(String.valueOf(HunkleberryGeneral.pdfFile.getName()))).append(": /").append(base).append(" Another plugin is using this command.").toString());
-			return;
-		} else
-		{
+		if (event.isCancelled()) {
+			log.warning((new StringBuilder(String.valueOf(HunkleberryGeneral.pdfFile.getName()))).append(": /").append(base)
+					.append(" Another plugin is listening for this command, consider creating an alias.").toString());
+		} else {
 			event.setCancelled(true);
-			return;
 		}
 	}
 }
