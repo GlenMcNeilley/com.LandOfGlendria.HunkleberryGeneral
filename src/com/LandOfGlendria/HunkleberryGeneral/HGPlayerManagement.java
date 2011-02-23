1a21f15716c84f86b456ba8af65a34bff21b2a08
GlenMcNeilley <glenmcn@gmail.com>
2011-02-23 12:09:21 -0700

    move leap here

--- src/com/LandOfGlendria/HunkleberryGeneral/HGPlayerManagement.java
+++ src/com/LandOfGlendria/HunkleberryGeneral/HGPlayerManagement.java
@@ -1,5 +1,7 @@
 package com.LandOfGlendria.HunkleberryGeneral;
 
+//TODO get list of stand-on-able blocks
+
 import java.util.*;
 import java.util.logging.Logger;
 import org.bukkit.*;
@@ -7,72 +9,57 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
-public class HGPlayerManagement
-{
+public class HGPlayerManagement {
 
 	@SuppressWarnings("unused")
 	private final Logger log = Logger.getLogger("Minecraft");
 	private HGMessageManagement msg;
 
-	public HGPlayerManagement(HGMessageManagement msg)
-	{
+	public HGPlayerManagement(HGMessageManagement msg) {
 		this.msg = msg;
 	}
 
-	public String healPlayer(Player player)
-	{
+	public String healPlayer(Player player) {
 		player.setHealth(HGStatics.MAX_HEALTH.intValue());
 		return null;
 	}
 
-	public String setDisplayName(Player player, String name, String colorArray)
-	{
-		StringBuffer displayName = new StringBuffer((new StringBuilder(" ")).append(HGStatics.NO_COLOR).append(name).toString());
+	public String setDisplayName(Player player, String name, String colorArray) {
+		StringBuffer displayName = new StringBuffer();
+		displayName.append("");
+		displayName.append(HGStatics.NO_COLOR);
+		displayName.append(name);
 		int color = 0;
 		int index = 0;
 		int cumulative = 3;
-		if(colorArray != null)
-		{
+		if (colorArray != null) {
 			String as[];
 			int j = (as = colorArray.split("/")).length;
-			for(int i = 0; i < j; i++)
-			{
+			for (int i = 0; i < j; i++) {
 				String indexAndColor = as[i];
-				if(indexAndColor != null)
-				{
+				if (indexAndColor != null) {
 					String pair[] = indexAndColor.split(",");
-					if(pair.length == 2)
-					{
-						try
-						{
+					if (pair.length == 2) {
+						try {
 							index = Integer.parseInt(pair[0]);
-							if(index < 1 || index > name.length())
-							{
+							if (index < 1 || index > name.length()) {
 								return msg.formatInvalidArgs(index, "Index out of bounds");
 							}
-						}
-						catch(NumberFormatException e)
-						{
+						} catch (NumberFormatException e) {
 							return msg.formatInvalidArgs(pair[0], "Invalid index");
 						}
-						try
-						{
+						try {
 							color = Integer.parseInt(pair[1]);
-						}
-						catch(NumberFormatException e)
-						{
+						} catch (NumberFormatException e) {
 							return msg.formatInvalidArgs(pair[1], "Invalid color");
 						}
-						if(ChatColor.getByCode(color) != null)
-						{
+						if (ChatColor.getByCode(color) != null) {
 							displayName.insert((index + cumulative) - 1, ChatColor.getByCode(color));
 							cumulative += 2;
-						} else
-						{
+						} else {
 							return msg.formatInvalidArgs(color, "Invalid color");
 						}
-					} else
-					{
+					} else {
 						return msg.formatInvalidArgs(indexAndColor, "Invalid index.color pair");
 					}
 				}
@@ -86,18 +73,15 @@ public class HGPlayerManagement
 		return null;
 	}
 
-	public Block getFloor(Block block)
-	{
+	public Block getFloor(Block block) {
 		Block testingBlock = block;
 		Location loc = null;
-		int blockHeight = (int)block.getLocation().getY();
-		for(int height = blockHeight; height > 3; height--)
-		{
+		int blockHeight = (int) block.getLocation().getY();
+		for (int height = blockHeight; height > 3; height--) {
 			loc = testingBlock.getLocation();
 			loc.setY(height);
 			testingBlock = block.getWorld().getBlockAt(loc);
-			if(testingBlock.getFace(BlockFace.DOWN).getTypeId() != Material.AIR.getId())
-			{
+			if (testingBlock.getFace(BlockFace.DOWN).getTypeId() != Material.AIR.getId()) {
 				return testingBlock.getFace(BlockFace.UP);
 			}
 		}
@@ -105,73 +89,85 @@ public class HGPlayerManagement
 		return null;
 	}
 
-	public String teleport(Player player, double x, double y, double z)
-	{
+
+	public String leap(Player player, Location location) {
+		msg.sendPositiveMessage(player, ("Leaping to " + location.getWorld().getName() + " (" +
+				(int) location.getX() + "," + 
+				(int) location.getY() + "," + 
+				(int) location.getZ() + ")."));
+		player.teleportTo(location);
+		return null;
+	}
+	
+	public String teleport(Player player, double x, double y, double z) {
 		player.teleportTo(new Location(player.getWorld(), x, y, z));
 		return null;
 	}
 
-	public String getLocation(Player player)
-	{
+	public String getLocation(Player player) {
 		Location loc = player.getLocation();
-		msg.sendPositiveMessage(player, (new StringBuilder("Loc: ")).append(loc.getX()).append(",").append(loc.getY()).append(",").append(loc.getZ()).append(").").toString());
+		msg.sendPositiveMessage(player, (new StringBuilder("Loc: ")).append(loc.getX()).append(",").append(loc.getY()).append(",").append(loc.getZ())
+				.append(").").toString());
 		return null;
 	}
 
-	public String stratum(Player player, int strataToJump)
-	{
+	public String stratum(Player player, int strataToJump) {
 		HashSet<Byte> blocks = new HashSet<Byte>();
-		Material amaterial[];
-		int strataCount = (amaterial = Material.values()).length;
-		for(int i = 0; i < strataCount; i++)
-		{
-			Material mat = amaterial[i];
-			blocks.add(Byte.valueOf((byte)mat.getId()));
+		HashSet<Byte> air = new HashSet<Byte>();
+		for (Material mat : Material.values()) {
+			blocks.add(Byte.valueOf((byte) mat.getId()));
 		}
+		air.add(Byte.valueOf((byte) Material.AIR.getId()));
 
-		List<Block> blockList = player.getLineOfSight(blocks, 250);
 		Location loc = null;
-		strataCount = 1;
-		boolean solidFound = false;
-		Iterator<Block> iterator = blockList.iterator();
-		while(iterator.hasNext()) 
-		{
-			Block block = (Block)iterator.next();
-			if(block.getY() < 3 || block.getY() > 123)
-			{
-				break;
-			}
-			if(solidFound && block.getTypeId() == Material.AIR.getId())
-			{
-				if(strataCount == strataToJump)
-				{
-					if(block.getFace(BlockFace.UP).getTypeId() != Material.AIR.getId())
-					{
-						continue;
-					}
-					loc = getFloor(block).getFace(BlockFace.DOWN).getLocation();
-					if(loc != null)
-					{
-						break;
-					}
-				} else
-				{
-					solidFound = false;
-					strataCount++;
+		if (strataToJump > 0) {
+			List<Block> blockList = player.getLineOfSight(blocks, HGStatics.MAX_DISTANCE);
+			int strataCount = 1;
+			boolean solidFound = false;
+			Iterator<Block> iterator = blockList.iterator();
+			while (iterator.hasNext()) {
+				Block block = (Block) iterator.next();
+				if (block.getY() < 3 || block.getY() > 123) {
+					break;
 				}
-			} else
-			if(block.getTypeId() != Material.AIR.getId() && block.getTypeId() != Material.LAVA.getId() && block.getTypeId() != Material.STATIONARY_LAVA.getId() && block.getType().isBlock())
-			{
-				solidFound = true;
+				if (solidFound && block.getTypeId() == Material.AIR.getId()) {
+					if (strataCount == strataToJump) {
+						if (block.getFace(BlockFace.UP).getTypeId() != Material.AIR.getId()) {
+							continue;
+						}
+						loc = getFloor(block).getFace(BlockFace.DOWN).getLocation();
+						if (loc != null) {
+							break;
+						}
+					} else {
+						solidFound = false;
+						strataCount++;
+					}
+				} else if (block.getTypeId() != Material.AIR.getId() && block.getTypeId() != Material.LAVA.getId()
+						&& block.getTypeId() != Material.STATIONARY_LAVA.getId() && block.getType().isBlock()) {
+					solidFound = true;
+				}
+			}
+		} else {
+			List<Block> blockList = player.getLineOfSight(air, HGStatics.MAX_DISTANCE);
+			Block block = null;
+			for (int i = blockList.size()-2; i > -1 ; i-- ) {
+				block = blockList.get(i);
+				loc = getFloor(block).getFace(BlockFace.DOWN).getLocation();
+				if (loc != null) {
+					break;
+				}
 			}
 		}
-		if(loc != null)
-		{
+		if (loc != null) {
+			loc.setPitch(player.getLocation().getPitch());
+			loc.setYaw(player.getLocation().getYaw());
+			loc.setX(loc.getX()+.5);
+			loc.setZ(loc.getZ()+.5);
 			msg.sendPositiveMessage(player, "Air found, going there.");
 			player.teleportTo(loc);
 			return null;
-		} else
-		{
+		} else {
 			return "Unable to find air.";
 		}
 	}
