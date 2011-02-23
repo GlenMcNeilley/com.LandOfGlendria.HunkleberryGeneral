78250ebf2ed82b072fb4f27694e50823de5292b3
GlenMcNeilley <glenmcn@gmail.com>
2011-02-22 20:12:34 -0700

    formatting, world env touch file

--- src/com/LandOfGlendria/HunkleberryGeneral/HGWorldManagement.java
+++ src/com/LandOfGlendria/HunkleberryGeneral/HGWorldManagement.java
@@ -1,6 +1,7 @@
 package com.LandOfGlendria.HunkleberryGeneral;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import net.minecraft.server.EntityItem;
@@ -10,6 +11,7 @@ import org.bukkit.craftbukkit.entity.CraftItem;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.*;
 import org.bukkit.plugin.Plugin;
+
 
 public class HGWorldManagement {
 
@@ -40,7 +42,7 @@ public class HGWorldManagement {
 				killcount++;
 			}
 		}
-		msg.sendPositiveMessage(player, (new StringBuilder("Removed ")).append(killcount).append(" living entities.").toString());
+		msg.sendPositiveMessage(player, ("Removed " + killcount + " living entities."));
 		return null;
 	}
 
@@ -59,7 +61,7 @@ public class HGWorldManagement {
 			}
 		}
 
-		msg.sendPositiveMessage(player, (new StringBuilder("Removed ")).append(killcount).append(" item entities.").toString());
+		msg.sendPositiveMessage(player, ("Removed " + killcount + " item entities."));
 		return null;
 	}
 
@@ -82,49 +84,123 @@ public class HGWorldManagement {
 		msg.sendSegmented(player, sb.toString());
 		return null;
 	}
+	
+	public synchronized String worldLoader(Player player, HGCommandData command, String name, String env) {
+		if (command == HGCommandData.LOAD_WORLD) {
+			return loadWorld(player,name);
+		} else if (command == HGCommandData.CREATE_WORLD) {
+			return createWorld(player,name,env);
+		} else if (command == HGCommandData.DECLARE_WORLD_ENV) {
+			return createWorldEnvFile(player,name,env);
+		} else {
+			return ("Internal error. Invalid command to worldLoader. Command = " + command.getCommand());
+		}
+	}
 
-	public String loadWorld(Player player, HGCommandData command, String name, String env) {
-		boolean exists = false;
-		Enum<World.Environment> envEnum;
+	private String loadWorld(Player player, String name) {
+		World.Environment envEnum =  World.Environment.NORMAL;
+		File worldFile = new File(name + "/" + HGStatics.WORLD_DATA_FILE);
+		File netherFile = new File(name + "/" + HGStatics.NETHER_ENV_FILE);
+		File normalFile = new File(name + "/" + HGStatics.NORMAL_ENV_FILE);
+		if (!worldFile.exists()) {
+			msg.warn(player.getName() + " attempted to load a world that doesn't exist [" + name + "].");
+			return (name + "does not exist.");
+		}
+		
+		if (netherFile.exists() && normalFile.exists()) {
+			msg.warn(player.getName() + " attempted to load world " + name + " but it has both NORMAL and NETHER environment files.  Please delete the appropriate file.");
+			return ("Unable to load environment file for " + name + ".  See the server console for more information.");
+		} else if (netherFile.exists()) {
+			envEnum = World.Environment.NETHER;
+		} else if (normalFile.exists()) {
+			envEnum = World.Environment.NORMAL;
+		} else {
+			msg.warn(player.getName() + " attempted to load world " + name + " but it does not have a NORMAL or NETHER environment file.  Use /setenvtype to correct this issue.");
+			return ("Unable to load environment file for " + name + ".  See the server console for more information.");		
+		}
+		
+		msg.sendPositiveMessage(player, ("Loading ") + envEnum.toString() + " world " + name + ". Please wait for completion message.");
+		World created = plugin.getServer().createWorld(name, envEnum);
+		if (created != null) {
+			msg.sendPositiveMessage(player, (envEnum.toString() + " world " + name + " has loaded and is available."));
+			return null;
+		} else {
+			return ("Loading of " + envEnum.toString() + " world " + name + " has been refused by the server for an unknown reason.");
+		}
+	}
+	
+	
+	private String createWorld(Player player, String name, String env) {
+		World.Environment envEnum =  World.Environment.NORMAL;
+		File worldFile = new File(name + "/" + HGStatics.WORLD_DATA_FILE);
+		if (worldFile.exists()) {
+			msg.warn(player.getName() + " attempted to create a world that already exists [" + name + "].");
+			return (name + " already exists.");
+		}
+
 		if (env.equalsIgnoreCase("nether")) {
-			envEnum = org.bukkit.World.Environment.NETHER;
+			envEnum = World.Environment.NETHER;
 		} else if (env.equalsIgnoreCase("normal")) {
-			envEnum = org.bukkit.World.Environment.NORMAL;
+			envEnum = World.Environment.NORMAL;
 		} else {
 			return msg.formatInvalidArgs(env, "Invalid environment");
 		}
-		File file = new File((new StringBuilder()).append(new StringBuilder(String.valueOf(name))).append("/level.dat").toString().toString());
-		if (file.exists()) {
-			exists = true;
-		}
-		if (exists) {
-			if (command == HGCommandData.CREATE_WORLD) {
-				return (new StringBuilder("Unable to create world. File ")).append(file.getName()).append(" already exists.").toString();
-			}
-			plugin.getServer().createWorld(name, (org.bukkit.World.Environment) envEnum);
-			msg.sendPositiveMessage(player, (new StringBuilder("Loading ")).append(env).append(" world ").append(name).append(".").toString());
+
+		msg.sendPositiveMessage(player, ("Creating ") + envEnum.toString() + " world " + name + ". Please wait for completion message (may take a moment).");
+		World created = plugin.getServer().createWorld(name, envEnum);
+		if (created != null) {
+			createWorldEnvFile(player,name,env);
+			msg.sendPositiveMessage(player, (envEnum.toString() + " world " + name + " has been created and is available."));
+			return null;
 		} else {
-			if (command == HGCommandData.LOAD_WORLD) {
-				return (new StringBuilder("Unable to load world. File ")).append(file.getName()).append(" doesn't exist.").toString();
-			}
-			plugin.getServer().createWorld(name, (org.bukkit.World.Environment) envEnum);
-			msg.sendPositiveMessage(player, (new StringBuilder("Creating ")).append(env).append(" world ").append(name).append(".").toString());
+			return ("Creating of " + envEnum.toString() + " world " + name + " has been refused by the server for an unknown reason.");
 		}
-		return null;
 	}
+
+	private String createWorldEnvFile(Player player, String name, String env) {
+		File worldFile = new File(name + "/" + HGStatics.WORLD_DATA_FILE);
+		File netherFile = new File(name + "/" + HGStatics.NETHER_ENV_FILE);
+		File normalFile = new File(name + "/" + HGStatics.NORMAL_ENV_FILE);
+		if (!worldFile.exists()) {
+			msg.warn(player.getName() + " attempted to access a world that doesn't exist [" + name + "].");
+			return (name + "does not exist.");
+		}
+		if (netherFile.exists() || normalFile.exists()) {
+			msg.warn(player.getName() + " attempted to create an environment file for a world that already has one [" + name + "].");
+			return (name + " already has an environment file.");
+		}
+		try {
+			if (env.equalsIgnoreCase("nether")) {
+				netherFile.createNewFile();
+				msg.sendPositiveMessage(player, ("Created environment file " + netherFile.getPath() + "."));
+				return null;
+			} else if (env.equalsIgnoreCase("normal")) {
+				normalFile.createNewFile();
+				msg.sendPositiveMessage(player, ("Created environment file " + normalFile.getPath() + "."));
+				return null;
+			} else {
+				return msg.formatInvalidArgs(env, "Invalid environment");
+			}
+		} catch (IOException e) {
+			msg.warn(player.getName() + " System level error creating world environment file for [" + name + "].");
+			return ("Operating system has refused to create world environment file for name" + name +".");
+		}
+	}
+	
 
 	public String setSpawnLocation(Player player, World world, int x, int y, int z) {
 		((CraftWorld) world).getHandle().spawnX = x;
 		((CraftWorld) world).getHandle().spawnY = y;
 		((CraftWorld) world).getHandle().spawnZ = z;
-		msg.sendPositiveMessage(player, (new StringBuilder("Set spawn point in ")).append(world.getName()).append(" to (").append(x).append(",").append(y)
-				.append(",").append(z).append(").").toString());
+		msg.sendPositiveMessage(player, ("Set spawn point in " + world.getName() + " to (" + x + "," + y + "," + z + ")."));
 		return null;
 	}
 
 	public String leap(Player player, Location location) {
-		msg.sendPositiveMessage(player, (new StringBuilder("Leaping to ")).append(location.getWorld().getName()).append(" (")
-				.append((int) location.getX()).append(",").append((int) location.getY()).append(",").append((int) location.getZ()).append(").").toString());
+		msg.sendPositiveMessage(player, ("Leaping to " + location.getWorld().getName() + " (" +
+				(int) location.getX() + "," + 
+				(int) location.getY() + "," + 
+				(int) location.getZ() + ")."));
 		player.teleportTo(location);
 		return null;
 	}
